3076fe8534e4ff0f5075a546574acaabd45a02b0
harsh2799 <mishraharsh27@gmail.com>
2023-01-21 14:48:14 -0500

    Added firebase code

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template, make_response
 from flask_restful import Api, Resource
+import json
+import firebase_admin
+from firebase_admin import credentials, db, firestore
+
+cred = credentials.Certificate("static\\files\\conuhacksnbcvisualize-firebase-adminsdk-cizk4-13c381ff83.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+db_ref = db.collection('nbc_db')
+
 
 app = Flask(__name__)
 api = Api(app)
-
-
 
 class Temp(Resource):
 	def get(self):
@@ -14,6 +21,7 @@ class Temp(Resource):
 api.add_resource(Temp, '/')
 
 if __name__ == '__main__':
+	
 	app.run(
 		host='localhost',
 		port=5000,
