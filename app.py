7c713d484e71539f7998c2cbe757361a00851397
harsh2799 <mishraharsh27@gmail.com>
2023-01-21 22:36:57 -0500

    Added code to load data into dataframe

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, make_response
 from flask_restful import Api, Resource
-import json
-import firebase_admin
-from firebase_admin import credentials, db, firestore
+# import json
+# import firebase_admin
+# from firebase_admin import credentials, db, firestore
+from business_logic.pre_processing import Exchange
 
-cred = credentials.Certificate("static\\files\\conuhacksnbcvisualize-firebase-adminsdk-cizk4-13c381ff83.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
-db_ref = db.collection('nbc_db')
-
+# cred = credentials.Certificate("static\\files\\conuhacksnbcvisualize-firebase-adminsdk-cizk4-13c381ff83.json")
+# firebase_admin.initialize_app(cred)
+# db = firestore.client()
+# db_ref = db.collection('nbc_db')
 
 app = Flask(__name__)
 api = Api(app)
@@ -18,10 +18,14 @@ class Temp(Resource):
 		print("Get")
 		return make_response(render_template('index.html'))
 
+	def post(self):
+		return data
+
 api.add_resource(Temp, '/')
 
 if __name__ == '__main__':
-	
+	data = Exchange().process_index()
+	print(data['tsx'])
 	app.run(
 		host='localhost',
 		port=5000,
---
+++ business_logic/pre_processing.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+indices = [
+			{"name": "alpha", "filename": "AlphaData"},
+			{"name": "tsx", "filename": "TSXData"},
+			{"name": "aequitas", "filename": "AequitasData"}
+		]
+
+class Exchange:
+
+	
+
+	def process_exchange_data(self, df):
+		df = df.groupby(["Symbol", "OrderID", "MessageType"]).first()
+		nested_dict = {}
+		temp_dict = df.to_dict(orient='index')
+		for key1, value1 in temp_dict.items():
+			if key1[0] not in nested_dict:
+				nested_dict[key1[0]] = {}
+			if key1[1] not in nested_dict[key1[0]]:
+				nested_dict[key1[0]][key1[1]] = {}
+			nested_dict[key1[0]][key1[1]][key1[2]] = value1
+		
+		return nested_dict
+		# for key in nested_dict.keys():
+		#     db_ref.document(name).set({key: nested_dict[key]}, merge=True)
+
+	def process_index(self):
+		global indices
+		data = {}
+		for index in indices:
+			data[index["name"]] = self.process_exchange_data(pd.read_json(f"D:\Git Repositories\ConUHacksNBCVisualize\static\Hackathon\Hackathon\{index['filename']}.json"))
+
+		return data
